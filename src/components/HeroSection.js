cde59959b370df2a09a7081ad9e4f030e08be731
Moses <odhismoses20@gmail.com>
2023-06-11 20:08:13 +0300

    Test commit

---
+++ src/components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h2>Welcome to My Landing Page. I guess live reload is working</h2>
+      <p>This is a basic landing page created using React.</p>
+      {/* Add other content or call-to-action button here */}
+    </section>
+  );
+};
+
+export default HeroSection;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1>My Landing Page</h1>
+      {/* Add navigation links here */}
+    </nav>
+  );
+};
+
+export default Navbar;
